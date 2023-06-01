5d40c26b5e7fdb20c998f9ec505ffac0435d54a4
[Kenney Kong] <[kenneykong@gmail.com]>
2023-05-31 20:01:20 -0400

    created App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import data from "./data"
+import Header from "./Header"
+ 
+export default App = () => {
+  const [currentSelected, setCurrentSelectedPosition] = useState('pg')
+  const handleClick = (val) => () => {setCurrentSelectedPosition(val)}
+  return(
+      <div>
+        <Header />
+      <b>'POSITION'</b>
+      <span id='position'>
+        <a onClick={handleClick("pg")} id="pg" href="#posPG">PG</a>
+        <a onClick={handleClick("sg")} id="sg" href="#posSG">SG</a>
+        <a onClick={handleClick("sf")} id="sf" href="#posSF">SF</a>
+        <a onClick={handleClick("pf")} id="pf" href="#posPF">PF</a>
+        <a onClick={handleClick("ct")} id="ct" href="#posC">C</a>
+      </span>
+      {currentSelected === 'pg' && <div>
+            <span>Point Guards are </span>
+            <div>
+              <p>Trevor Hudgins <b>PG</b></p>
+              <p>Daishen Nix <b>PG</b></p>
+              <p>Kevin Porter Jr <b>PG/SG</b></p>
+              <p>TyTy Washington <b>PG</b></p>
+          
+            </div>       
+      </div>}
+      {currentSelected === 'sg' && <div>List Shooting Guards here</div>}
+      {currentSelected === 'sf' && <div>List Small Fowards here</div>}
+      {currentSelected === 'pf' && <div>List Power Forwards here</div>}
+      {currentSelected === 'ct' && <div>List Centers here</div>}
+  </div>
   );
 }
-
-export default App;
