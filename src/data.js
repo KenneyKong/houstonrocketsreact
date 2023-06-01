47408133e112998a711ffc136337d08c12c7310d
[Kenney Kong] <[kenneykong@gmail.com]>
2023-05-31 20:01:42 -0400

    created data.js

---
+++ src/data.js
@@ -0,0 +1,66 @@
+export default [
+    {
+        name: "Trevor Hudgins",
+        position: "PG"
+    },
+    {
+        name: "Daishen Nix",
+        position: "PG"
+    },
+    {
+        name: "Kevin Porter Jr",
+        position: "PG",
+    },
+    {
+        name: "TyTy Washington",
+        position: "PG"
+    },
+    {
+        name: "Josh Christopher",
+        position: "SG"
+    },
+    {
+        name: "Jalen Green",
+        position: "SG"
+    },
+    {
+        name: "Darius Days",
+        position: "SF"
+    },
+    {
+        name: "Tari Eason",
+        position: "SF"
+    },
+    {
+        name: "Kenyon Martin Jr",
+        position: "SF"
+    },
+    {
+        name: "Jae'sean Tate",
+        position: "SF"
+    },
+    {
+        name: "Usman Garuba",
+        position: "PF"
+    },
+    {
+        name: "Frank Kaminsky",
+        position: "PF"
+    },
+    {
+        name: "Jabari Smith Jr",
+        position: "PF"
+    },
+    {
+        name: "Willie Cauley-Stein",
+        position: "C"
+    },
+    {
+        name: "Boban Marjanovic",
+        position: "C"
+    },
+    {
+        name: "Alperen Sengun",
+        position: "C"
+    }
+]
